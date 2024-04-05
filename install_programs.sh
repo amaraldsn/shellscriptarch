67bfd87e9709015db7d79c4db87098e986700ffd
amaraldzn <amaral.https@gmail.com>
2024-04-05 10:56:14 -0300

    teste 2

--- install_programs.sh
+++ install_programs.sh
@@ -10,6 +10,59 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 
 # Lista de pacotes
 pacotes=(
+  #pacman
+  chromium
+  arduino
+  deepin-calculator
+  gimp
+  inkscape
+
+  #flatpak
+  obs.flatpak
+)
+
+# Função para instalar um pacote
+function instalar_pacote() {
+  grupo="$1"
+  nome="$2"
+
+  echo "Instalando $nome ($grupo)..."
+
+  # Se o pacote termina em ".flatpak", instalar como Flatpak
+  if [[ "$nome" =~ .*\.flatpak$ ]]; then
+    flatpak install --user --noninteractive "$nome"
+    return
+  fi
+
+  # Instalar pacote do repositório padrão
+  sudo pacman -Sy "$nome" --noconfirm --quiet
+}
+
+# Percorrer a lista de pacotes e instalar cada pacote
+for pacote in "${pacotes[@]}"; do
+  # Divida o pacote em nome do grupo e nome do pacote
+  grupo="${pacote%%:*}"
+  nome="${pacote#*:}"
+
+  # Instalar o pacote
+  instalar_pacote "$grupo" "$nome"
+done
+
+echo "Instalação concluída!"
+
+'''
+#!/bin/bash
+
+# Permitir execução com sudo
+if [[ $(whoami) != "root" ]]; then
+  exec sudo -u root "$0" "$@"
+fi
+
+# Adicionar o repositório Flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+# Lista de pacotes
+pacotes=(
   "navegadores:chromium"
   "desenvolvimento:arduino"
   "ferramentas:deepin-calculator gimp inkscape"
@@ -44,6 +97,6 @@ for pacote in "${pacotes[@]}"; do
 done
 
 echo "Instalação concluída!"
-
+'''
 
 
