0e0684baa5c902d523b0217588e514cf92c9549a
amaraldzn <amaral.https@gmail.com>
2024-04-05 20:29:48 -0300

    final

--- install_flatpak.sh
+++ install_flatpak.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
-# Função para instalar um pacote Flatpak
+# Função para instalar o Flatpak
 function instalar_flatpak() {
+  echo "Instalando o Flatpak..."
+
+  # Atualizar o repositório de pacotes
+  sudo pacman -Syu
+
+  # Instalar o Flatpak e seus pré-requisitos
+  sudo pacman -S flatpak --noconfirm --quiet
+
+  # Habilitar o daemon do Flatpak
+  sudo systemctl enable --now flatpak.service
+
+  echo "Flatpak instalado com sucesso!"
+}
+
+# Função para instalar um pacote Flatpak
+function instalar_flatpak_app() {
   nome="$1"
 
   echo "Instalando $nome..."
@@ -12,17 +28,81 @@ function instalar_flatpak() {
   echo "$nome instalado com sucesso!"
 }
 
-# Lista de apps Flatpak para instalar
+# Lista de apps Flatpak
 apps=(
-  "obs.flatpak"
+  #NAVEGADORES
+  "com.brave.Browser"
+  "org.mozilla.firefox"
+  "com.microsoft.Edge"
+  "ru.yandex.Browser"
+  #MULTIMÍDIA
+  "io.github.Figma_Linux.figma_linux"
+  "com.rawtherapee.RawTherapee"
+  "com.obsproject.Studio"
   "com.spotify.Client"
-  # Adicione outros apps Flatpak aqui...
+  "org.libreoffice.LibreOffice"
+  "org.telegram.desktop"
+  #PROGRAMAÇÃO
+  "com.visualstudio.code"
+  #JOGOS
+  "com.valvesoftware.Steam"
+  "org.ppsspp.PPSSPP"
+  #ESTUDOS
+  "net.ankiweb.Anki"
 )
 
-# Percorrer a lista de apps e instalar cada app
-for app in "${apps[@]}"; do
-  instalar_flatpak "$app"
-done
+# Perguntar se deseja instalar o Flatpak
+echo "Você quer instalar o Flatpak? (S/N/0 para abortar):"
+read resposta
 
-echo "Instalação de apps Flatpak concluída!"
+# Instalar o Flatpak se a resposta for "S"
+if [[ "$resposta" =~ ^[Ss]$ ]]; then
+  instalar_flatpak
+fi
 
+# Verificar se o Flatpak já está instalado se a resposta for "N"
+if [[ "$resposta" =~ ^[Nn]$ ]]; then
+  if ! command -v flatpak &> /dev/null; then
+    echo "O Flatpak não está instalado. Abortando..."
+    exit 1
+  fi
+fi
+
+# Perguntar se deseja instalar todos os programas
+echo "Você quer instalar todos os programas? (S/N/0 para abortar):"
+read resposta
+
+# Instalar todos os programas se a resposta for "S"
+if [[ "$resposta" =~ ^[Ss]$ ]]; then
+  for app in "${apps[@]}"; do
+    instalar_flatpak_app "$app"
+  done
+  echo "Instalação concluída!"
+  exit 0
+fi
+
+# Perguntar se deseja instalar os programas separadamente se a resposta for "N"
+if [[ "$resposta" =~ ^[Nn]$ ]]; then
+  echo "Você deseja instalar os programas separadamente? (S/0 para abortar):"
+  read resposta
+
+  # Instalar os programas separadamente se a resposta for "S"
+  if [[ "$resposta" =~ ^[Ss]$ ]]; then
+    for app in "${apps[@]}"; do
+      echo "Deseja instalar o app $app (S/N)?"
+      read resposta
+
+      # Instalar app se a resposta for "S"
+      if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        instalar_flatpak_app "$app"
+      fi
+    done
+    echo "Instalação concluída!"
+  fi
+fi
+
+# Exibir mensagem de erro se a resposta for inválida
+if [[ "$resposta" != ^[SsNn0]$ ]]; then
+  echo "Resposta inválida. Abortando..."
+  exit 1
+fi
--- install_pacman.sh
+++ install_pacman.sh
@@ -1,54 +1,124 @@
 #!/bin/bash
 
-# Permitir execução com sudo
-if [[ $(whoami) != "root" ]]; then
-  exec sudo -u root "$0" "$@"
-fi
-
-# Adicionar o repositório Flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-# Lista de pacotes
-pacotes=(
-  #pacman
-  chromium
-  arduino
-  deepin-calculator
-  gimp
-  inkscape
-
-  #flatpak
-  com.obsproject.Studio
-  com.spotify.Client
-)
-
 # Função para instalar um pacote
 function instalar_pacote() {
-  grupo="$1"
-  nome="$2"
+ nome="$1"
 
-  echo "Instalando $nome ($grupo)..."
+ echo "Instalando $nome..."
 
-  # Se o pacote termina em ".flatpak", instalar como Flatpak
-  if [[ "$nome" =~ .*\.flatpak$ ]]; then
-    flatpak install --user --noninteractive "$nome"
-    return
-  fi
-
-  # Instalar pacote do repositório padrão
-  sudo pacman -Sy "$nome" --noconfirm --quiet
+ # Instalar pacote do repositório padrão
+ sudo pacman -Sy "$nome" --noconfirm --quiet
 }
 
-# Percorrer a lista de pacotes e instalar cada pacote
-for pacote in "${pacotes[@]}"; do
-  # Divida o pacote em nome do grupo e nome do pacote
-  grupo="${pacote%%:*}"
-  nome="${pacote#*:}"
+# Função para instalar um pacote via Yay
+function instalar_yay() {
+ nome="$1"
 
-  # Instalar o pacote
-  instalar_pacote "$grupo" "$nome"
-done
+ echo "Instalando $nome via Yay..."
 
-echo "Instalação concluída!"
+ # Instalar pacote via Yay
+ yay -S "$nome" --noconfirm --quiet
+}
 
+# Lista de pacotes do repositório oficial
+pacotes_repo=(
+ #NAVEGADOR
+ "firefox"
+ #MULTIMÍDIA
+ "gimp"
+ "inkscape"
+ #FERRAMENTAS-SISTEMA
+ "isoimagewriter"
+ "gnome-calculator"
+)
 
+# Lista de pacotes AUR
+pacotes_aur=(
+ #FERRAMENTAS
+ "kdeconnect"
+ #VIRTUALIZAÇÃO
+ "virt-manager"
+ #PROGRAMAÇAÕ
+ "github-desktop-bin"
+ "jdk"
+)
+
+# Perguntar se deseja instalar todos os programas
+echo "Você quer instalar todos os programas? (S/N/0 para abortar):"
+read resposta
+
+# Instalar todos os programas do repositório oficial se a resposta for "S"
+if [[ "$resposta" =~ ^[Ss]$ ]]; then
+ for pacote in "${pacotes_repo[@]}"; do
+   instalar_pacote "$pacote"
+ done
+
+ # Instalar o GitHub Desktop via Yay
+ instalar_yay "${pacotes_aur[0]}"
+
+ echo "Instalação concluída!"
+ exit 0
+fi
+
+# Perguntar se deseja instalar os programas separadamente se a resposta for "N"
+if [[ "$resposta" =~ ^[Nn]$ ]]; then
+ echo "Você deseja instalar os programas separadamente? (S/0 para abortar):"
+ read resposta
+
+ # Instalar os programas separadamente se a resposta for "S"
+ if [[ "$resposta" =~ ^[Ss]$ ]]; then
+   for pacote in "${pacotes_repo[@]}" "${pacotes_aur[@]}"; do
+      # Descrição breve do pacote (opcional)
+      descricao=""
+      case "$pacote" in
+        "firefox")
+          descricao="Segundo melhor navegador"
+          ;;
+        "kdeconnect")
+          descricao="Conecta seu smartphone ao seu computador."
+          ;;
+        "gimp")
+          descricao="Melhor editor de imagem"
+          ;;
+        "inkscape")
+          descricao="Segunda melhor alternativa para o I"
+          ;;
+        "isoimagewriter")
+          descricao="Criador de ISO"
+          ;;
+        "gnome-calculator")
+          descricao="Calculadora"
+          ;;
+        "virt-manager")
+          descricao="A melhor virtualização"
+          ;;
+        "github-desktop-bin")
+          descricao="Alternativa (software)"
+          ;;
+        "jdk")
+          descricao="Javinha pra jogar mine"
+          ;;
+        # ...
+      esac
+
+      echo "Deseja instalar o pacote $pacote ($descricao)? (S/N)"
+      read resposta
+
+      if [[ "$resposta" =~ ^[Ss]$ ]]; then
+        if [[ "$pacote" =~ ^aur- ]]; then
+          instalar_yay "$pacote"
+        else
+          instalar_pacote "$pacote"
+        fi
+      fi
+    done
+
+    echo "Instalação concluída!"
+  fi
+fi
+
+# Exibir mensagem de erro se a resposta for inválida
+if [[ "$resposta" != ^[SsNn0]$ ]]; then
+ echo "Resposta inválida. Abortando..."
+ exit 1
+fi
